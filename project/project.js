6a0207df8ad09bb301f263fb7fa6d3657a652df3
Ninjafio <148548020+Ninjafio@users.noreply.github.com>
2023-10-20 16:29:48 +0500

    Add files via upload

---
+++ project/project.js
@@ -0,0 +1,34 @@
+x = 0
+y = 0
+class Car{
+constructor(color,model, x, y){
+this.color = color
+this.model = model
+this.y = y
+this.x = x
+    }
+    moveRight(){
+        y++
+        console.log(`Машина едет вправо`)
+    }
+    moveLeft(){
+        y--
+        console.log(`Машина едет влево`)
+    }
+    moveForward(){
+        x++
+        console.log(`Машина едет вперед`)
+    }
+    moveBackward(){
+        x--
+        console.log(`Машина едет назад`)
+    }
+
+}
+let cars = [
+    new Car("Синий", "Матиз",0 , 0 ),
+    new Car("Красный", "Жигуль", 0 , 0 ),
+    new Car("Зеленый", "Запорожец",0 , 0 ),
+    new Car("Черный", "Волга", 0 , 0 )
+]
+
